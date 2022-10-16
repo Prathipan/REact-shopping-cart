621be8f154d955385dd5c2c900480e5c04ba0579
Prathipan <prathipan24p@gmail.com>
2022-10-17 01:19:56 +0530

    added cart empty

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,11 +3,11 @@ import { CartContext } from "../App";
 
 const Cart = ({ deleteFunc }) => {
   const { cartItems } = useContext(CartContext);
-  const [total,setTotal] = useState(0);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    setTotal(cartItems.reduce((acc,curr) => acc+ Number(curr.price),0));
-  },[cartItems])
+    setTotal(cartItems.reduce((acc, curr) => acc + Number(curr.price), 0));
+  }, [cartItems]);
   console.log(cartItems);
   const handleDelete = (id) => {
     console.log(id);
@@ -16,29 +16,34 @@ const Cart = ({ deleteFunc }) => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-lg-9">
-        {cartItems.map(({ id, value, price, imgUrl }, index) => {
-        return (
-            <div key={index} className="card">
-              <img className="cart-img" src={imgUrl} alt={value}></img>
-              <h3>{value}</h3>
-              <p>Rs.{price}</p>
-              <button
-                className="btn btn-sm btn-danger"
-                onClick={() => handleDelete(index)}
-              >
-                Remove from cart
-              </button>
+        {cartItems.length ? (
+          <>
+            <div className="col-lg-9">
+              {cartItems.map(({ id, value, price, imgUrl }, index) => {
+                return (
+                  <div key={index} className="card">
+                    <img className="cart-img" src={imgUrl} alt={value}></img>
+                    <h3>{value}</h3>
+                    <p>Rs.{price}</p>
+                    <button
+                      className="btn btn-sm btn-danger"
+                      onClick={() => handleDelete(index)}
+                    >
+                      Remove from cart
+                    </button>
+                  </div>
+                );
+              })}
             </div>
-        );
-      })}
-        </div>
-        <div className="col-lg-3">
-          <h2>Total : Rs.{total}</h2>
-          <button className="btn btn-success">Checkout</button>
-        </div>
+            <div className="col-lg-3">
+              <h2>Total : Rs.{total}</h2>
+              <button className="btn btn-success">Checkout</button>
+            </div>
+          </>
+        ) : (
+          <h2 className="text-center">Cart is Empty !!!</h2>
+        )}
       </div>
-      
     </div>
   );
 };
